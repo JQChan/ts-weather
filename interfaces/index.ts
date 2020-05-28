0448b2bdaede423981f15cc18c17b7165b0eb505
JQ_Chan <vikers28@qq.com>
2020-05-28 19:32:32 +0800

    定义接口返回数据

---
+++ interfaces/index.ts
@@ -0,0 +1,41 @@
+export interface IWeatherResponse{
+  status: string;
+  count: string;
+  info: string;
+  infocode: string;
+  lives?: ILive[];
+  forecasts?: IForecast[];
+}
+
+export interface ILive{
+  province: string;
+  city:string;
+  adcode: string;
+  weather: string;
+  temperature: string;
+  winddirection: string;
+  windpower: string;
+  humidity: string;
+  reporttime: string;
+}
+
+export interface IForecast{
+  city: string;
+  adcode: string;
+  province: string;
+  reporttime: string;
+  casts: ICast[];
+}
+
+export interface ICast{
+  date: string;
+  week: string;
+  dayweather: string;
+  nightweather: string;
+  daytemp: string;
+  nighttemp: string;
+  daywind: string;
+  nightwind: string;
+  daypower: string;
+  nightpower: string;
+}
